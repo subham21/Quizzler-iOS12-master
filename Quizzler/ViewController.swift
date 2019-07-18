fb5dc9e46340017a9ff3cc9b3b39b6b940b73107
subham sah <subham.tbipl@gmail.com>
2019-07-18 12:46:25 +0530

    Quizzler app complete

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Quizzler
 //
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by Subham Sah on 25/08/2015.
+//  Copyright (c) 2015 Subham. All rights reserved.
 //
 
 import UIKit
